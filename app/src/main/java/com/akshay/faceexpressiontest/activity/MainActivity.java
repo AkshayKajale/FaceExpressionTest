f306f2cdfade10864bc64eb3d7851129618db0b2
akshaykajale <aksh.kajale@gmail.com>
2020-11-30 12:21:05 -0800

    Model Loaded Successfully

--- app/src/main/java/com/akshay/faceexpressiontest/activity/MainActivity.java
+++ app/src/main/java/com/akshay/faceexpressiontest/activity/MainActivity.java
@@ -5,23 +5,25 @@ import android.os.Bundle;
 import com.akshay.faceexpressiontest.R;
 import androidx.appcompat.app.AppCompatActivity;
 import com.akshay.faceexpressiontest.fragment.FirstFragment;
-
-import org.opencv.android.JavaCamera2View;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 import org.tensorflow.lite.Interpreter;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Interpreter interpreter;
+    Interpreter tflite;
     private static final String MODEL_file = "file:///assets/tf_model.pb";
     private static final String INPUT_NODE = "conv2d_1_input_1:0";
     private static final String OUTPUT_NODE = "activation_5_1/Softmax:0";
     private static final long[] input_shape = {48,48,1};
     private TensorFlowInferenceInterface inferenceInterface;
     private boolean doneLoadingModel;
-    JavaCamera2View cameraView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +34,24 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, FirstFragment.getFragment())
                     .commit();
         }
-        inferenceInterface = new TensorFlowInferenceInterface(getAssets(), MODEL_file);
-        System.out.println("Model Loaded Successfully");
+        try {
+            tflite = new Interpreter(loadModelFile());
+            System.out.println("Model loaded Successfully");
+            System.out.println(tflite);
 
-
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 
-    private void loadModel(){
-
-        try{
-            inferenceInterface = new TensorFlowInferenceInterface(getAssets(), MODEL_file);
-            doneLoadingModel = true;
-        }
-        catch(IllegalArgumentException e){
-
-            doneLoadingModel = false;
-            e.printStackTrace();
-        }
-
-
+    private MappedByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor fileDescriptor=this.getAssets().openFd("model.tflite");
+        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel=inputStream.getChannel();
+        long startOffset=fileDescriptor.getStartOffset();
+        long declareLength=fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,declareLength);
     }
 
 }
+
